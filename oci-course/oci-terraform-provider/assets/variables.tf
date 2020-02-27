5c7d9b51d23548e9c46f11db14ec5e8901dd0318
Lucas Jellema <lucas.jellema@amis.nl>
2020-02-27 13:24:14 +0100

    .

--- oci-course/oci-terraform-provider/assets/variables.tf
+++ oci-course/oci-terraform-provider/assets/variables.tf
@@ -11,10 +11,6 @@ variable "compartment_id" {}
 # inherits values from Environment Variable TF_VAR_namespace
 variable "namespace" {}
 
-# this variable holds the name for the bucket that will be created; the default value is tf-bucket. This could be overridden through an Environment Variable called TF_VAR_lab_bucket_name
-variable "lab_bucket_name" {
-  default = "tf-bucket"
-}
 
 variable "tags" {
   type = map(string)
@@ -27,7 +23,7 @@ variable "tags" {
 # inherits values from value passed on command line -var tenancy_id=aa
 variable "tenancy_id" {  
   type = string
-  default_value = "ocid1.tenancy.oc1..aaaaaaaag7c7slwmlvsodyym662ixlsonnihko2igwpjwwe2egmlf3gg6okq"
+  default = "ocid1.tenancy.oc1..aaaaaaaag7c7slwmlvsodyym662ixlsonnihko2igwpjwwe2egmlf3gg6okq"
 }
 
 data "oci_identity_compartments" "all_compartments" {
@@ -35,7 +31,41 @@ data "oci_identity_compartments" "all_compartments" {
     compartment_id = "${var.tenancy_id}"
 }
 
-# report the response from the function call - by retrieving the content attribute from the resource
 output "all_compartments" {
-  value = "${oci_functions_invoke_function.invoke_hello_function.content}"
+  value = "${data.oci_identity_compartments.all_compartments}"
+}
+
+data "oci_identity_compartments" "lab_compartments" {
+    compartment_id = "${var.tenancy_id}"
+    # only retain the compartment called lab-compartment
+    filter {
+        name   = "name"
+        values  = [ "lab-compartment"]
+    }
+}
+
+output "lab_compartment" {
+  value = "${data.oci_identity_compartments.lab_compartment}"
+}
+
+locals {
+  # store the first (and only) compartment returned from the data source in the local variable
+  lab_compartment = data.oci_identity_compartments.lab_compartment.compartments[0]
+}
+
+output "lab_compartment_id" {
+  value = "${local.lab_compartment.id}"
+}
+
+# this is also allowed (an expression referencing an element in a list) - in output and data, but not in resouce properties
+output "lab_compartment_id2" {
+  value = "${data.oci_identity_compartments.lab_compartment.compartments[0].id}"
+}
+
+data "oci_identity_compartment" "lab_compartment" {
+  id = "${data.oci_identity_compartments.lab_compartment.compartments[0].id}"
+}
+
+output "lab_compartment_id3" {
+  value = "${data.oci_identity_compartment.lab_compartment}"
 }
