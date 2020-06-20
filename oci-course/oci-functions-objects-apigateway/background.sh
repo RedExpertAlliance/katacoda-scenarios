3f45eb1bf257b80920ff6f0f05d2091b57f2035f
Lucas Jellema <lucas.jellema@amis.nl>
2020-06-20 08:30:25 +0200

    install OCI CLI in foreground; added VS Code integration

--- oci-course/oci-functions-objects-apigateway/background.sh
+++ oci-course/oci-functions-objects-apigateway/background.sh
@@ -6,11 +6,7 @@
     touch  ~/.oci/oci_api_key.pem
 
 
-    # Install the OCI CLI in silent mode with default settings
-    curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh > install-oci-cli.sh
-    chmod +777 install-oci-cli.sh
-    sudo apt-get install python3-venv
-    sudo ./install-oci-cli.sh --accept-all-defaults
+
 
     # install Project Fn CLI
     curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh
--- oci-course/oci-functions-objects-apigateway/foreground.sh
+++ oci-course/oci-functions-objects-apigateway/foreground.sh
@@ -2,7 +2,11 @@ echo "Installing Project Fn - in the background"
 
 export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING=True
 
-
+    # Install the OCI CLI in silent mode with default settings
+    curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh > install-oci-cli.sh
+    chmod +777 install-oci-cli.sh
+    sudo apt-get install python3-venv
+    sudo ./install-oci-cli.sh --accept-all-defaults
 
 # add this line to ~/.profile - to make oci a recognized shell command
 echo 'oci() { /root/bin/oci "$@"; }' >> ~/.profile
