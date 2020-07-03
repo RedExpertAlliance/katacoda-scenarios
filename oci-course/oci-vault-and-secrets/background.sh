ff8389f304ad5cb17cddbe7c27e896a3f4bbad8b
Lucas Jellema <lucas.jellema@amis.nl>
2020-07-03 11:30:24 +0200

    monitoring

--- oci-course/oci-vault-and-secrets/background.sh
+++ oci-course/oci-vault-and-secrets/background.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
-# start Docker Container with OCI 
 mkdir ~/.oci
 touch  ~/.oci/config
 touch  ~/.oci/oci_api_key.pem
 
-
-
 # install Project Fn CLI
 curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh
-
-touch /root/allSetInBackground
-
 docker pull fnproject/node:latest
